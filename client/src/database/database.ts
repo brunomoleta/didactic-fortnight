3a29389f406ee6b93b5f23e62471bff7541c9abe
Bruno Moleta <brunomoleta@pm.me>
2023-11-03 17:53:33 -0300

    feat/createDatabase

---
+++ client/src/database/database.ts
@@ -0,0 +1,64 @@
+export const homeHero: string =
+  'A simple solution to complex tasks is coming soon';
+
+export const homeParagraph: string =
+  'Say goodbye to inefficient juggling of multiple apps, teams, and projects. Officelite is the new collaboration platform built with an intuitive interface to improve productivity.';
+
+export const buttonText = {
+  heroHome: 'Get Started',
+  subscriptionOption: '30 day free trial',
+  sendForm: 'Submit',
+};
+
+export const subscriptionOption = [
+  {
+    type: 'starter',
+    storage: '2 GB',
+    price: 'Free',
+    numberUsers: '3 devices',
+    text: [
+      'basic document collaboration',
+      'no ads',
+      'access to web app',
+      '100% encrypted communication',
+      '90 days account inactivity',
+      'terminal integration',
+    ],
+  },
+  {
+    type: 'pro',
+    price: '9.99',
+    numberUsers: '10 devices',
+    text: [
+      'ai controls and insights',
+      '50 GB cloud storage',
+      'access to web app',
+      '100% encrypted communication',
+      'files stored forever',
+      'terminal integration',
+    ],
+  },
+  {
+    type: 'pro',
+    price: '24.99',
+    numberUsers: '40 devices',
+    text: [
+      'ai controls and insights',
+      '200 GB cloud storage',
+      'access to web app',
+      '100% encrypted communication',
+      'files stored forever',
+      'terminal integration',
+    ],
+  },
+];
+
+export const launchDate = new Date('2023-05-24');
+
+export const countdown: string = `Coming ${launchDate}`;
+
+export const timeLeftForLaunch = ['days', 'hours', 'mins', 'secs'];
+
+export const signUpHero: string = 'Work smarter. Save time.';
+export const signUpParagraph: string =
+  'Easily manage your projects. Get on the list and receive in-app perks available only to early subscribers. We are moving into final development and getting ready for official launch soon.';
