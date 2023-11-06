6d6d5fcc64712e59e933a7fb00b0ddafc16cf639
Bruno Moleta <brunomoleta@pm.me>
2023-11-06 14:39:27 -0300

    refactor/subOption

--- client/src/components/SubOptions/SubOptions.tsx
+++ client/src/components/SubOptions/SubOptions.tsx
@@ -2,16 +2,34 @@ import React from 'react';
 import { subscriptionOption } from '../../database/database';
 import SubOption from '../subOption';
 import type { IsubscriptionOptions } from '../../../types/static';
+import { nanoid } from 'nanoid';
+import styled from 'styled-components';
+import rootSettings from '../../styled-components/root';
+
+const LiOption = styled.li<{ $ultimate?: boolean }>`
+  border: 1px solid ${rootSettings.colors.grey};
+  background-color: ${(props) =>
+    props.$ultimate ? 'hsl(228, 100%, 66%)' : 'white'};
+`;
+
+const CardList = styled.ol`
+  display: flex;
+  flex-flow: column;
+  gap: 32px;
+`;
 
 function SubOptions() {
   return (
-    <div>
-      {subscriptionOption.map((item:IsubscriptionOptions) => (
-        <>
-          <SubOption option={item}/>
-        </>
+    <CardList>
+      {subscriptionOption.map((item: IsubscriptionOptions) => (
+        <LiOption
+          $ultimate={item.type === subscriptionOption[2].type}
+          key={nanoid()}
+        >
+          <SubOption option={item} />
+        </LiOption>
       ))}
-    </div>
+    </CardList>
   );
 }
 
