cad14beb648b31908aaeabbb060f79b8d07bf239
Bruno Moleta <brunomoleta@pm.me>
2023-11-06 11:20:48 -0300

    feat/homePage content

--- client/src/pages/homepage/homepage.tsx
+++ client/src/pages/homepage/homepage.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import { homeHero, homeParagraph } from '../../database/database';
+import Logo from '../../components/Logo';
+import SubOptions from '../../components/SubOptions';
 
 function Homepage() {
-  return <div></div>;
+  return (
+    <>
+      <Logo/>
+      <h1>{homeHero}</h1>
+      <p>{homeParagraph}</p>
+      <SubOptions/>
+    </>
+  );
 }
 
 export default Homepage;
