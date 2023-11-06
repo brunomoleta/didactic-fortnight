4c1d8b7a3f52e096ddd79d822c0644e706261cf6
Bruno Moleta <brunomoleta@pm.me>
2023-11-06 11:14:36 -0300

    refactor/addOfficeRoutes to App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,13 @@
 import React from 'react';
 
-import './App.css';
 import GlobalStyles from './GlobalStyles';
-import ConfirmationPage from './pages/confirmationPage';
-
+import OfficeRoutes from './routes';
 
 function App() {
-  // Return the App component.
+
   return (
     <>
-      <ConfirmationPage/>
-      <div className="App">office</div>
+      <OfficeRoutes />
       <GlobalStyles />
     </>
   );
