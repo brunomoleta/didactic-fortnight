3d5957185be5a97612fbe4f92ddf4c6e2bbfb2bf
Bruno Moleta <brunomoleta@pm.me>
2023-11-07 16:33:48 -0300

    style/prettier

--- server/src/middlewares/users/verifyEmail.middleware.ts
+++ server/src/middlewares/users/verifyEmail.middleware.ts
@@ -1,18 +1,22 @@
-import {NextFunction, Request, Response} from "express";
+import { NextFunction, Request, Response } from "express";
 
-import User from '../../entities/users.entity';
-import { AppError } from '../../errors/AppError.errors';
-import { userRepo } from '../../repositories';
+import User from "../../entities/users.entity";
+import { AppError } from "../../errors/AppError.errors";
+import { userRepo } from "../../repositories";
 
-export const verifyUserEmail = async (request: Request, response: Response, next: NextFunction): Promise<void> => {
-  const {email} = request.body
+export const verifyUserEmail = async (
+  request: Request,
+  response: Response,
+  next: NextFunction
+): Promise<void> => {
+  const { email } = request.body;
 
-  const userEmail: User | null = await userRepo.findOneBy({email})
+  const userEmail: User | null = await userRepo.findOneBy({ email });
 
-  const errorMessage:string = `Email already exists`
-  if(userEmail) {
-    throw new AppError(errorMessage, 409)
+  const errorMessage: string = `Email already exists`;
+  if (userEmail) {
+    throw new AppError(errorMessage, 409);
   }
 
-  return next()
-}
+  return next();
+};
