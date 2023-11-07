844e58ee713cc527ca8da268d1f6cb811fd924f2
Bruno Moleta <brunomoleta@pm.me>
2023-11-07 11:21:28 -0300

    feat/create middlewares

---
+++ server/src/middlewares/users/verifyEmail.middleware.ts
@@ -0,0 +1,18 @@
+import {NextFunction, Request, Response} from "express";
+
+import User from '../../entities/users.entity';
+import { AppError } from '../../errors/AppError.errors';
+import { userRepo } from '../../repositories';
+
+export const verifyUserEmail = async (request: Request, response: Response, next: NextFunction): Promise<void> => {
+  const {email} = request.body
+
+  const userEmail: User | null = await userRepo.findOneBy({email})
+
+  const errorMessage:string = `Email already exists`
+  if(userEmail) {
+    throw new AppError(errorMessage, 409)
+  }
+
+  return next()
+}
