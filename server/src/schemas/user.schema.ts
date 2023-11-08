db2c78be35dad59747308dfbf75f9372512f2414
Bruno Moleta <brunomoleta@pm.me>
2023-11-08 15:59:56 -0300

    refactor/userSchema plan

--- server/src/schemas/user.schema.ts
+++ server/src/schemas/user.schema.ts
@@ -1,13 +1,18 @@
 import { z } from "zod";
 
-const AllowedValues = z.enum(["starter", "pro", "ultimate"]);
+enum PlanOptions {
+  Q = "starter",
+  J = "business",
+  K = "enterprise"
+}
+
 export const userSchema = z.object({
   id: z.number().positive().int(),
   name: z.string().max(64).min(3),
   email: z.string().email().max(64).min(9),
   phoneNumber: z.string().max(24),
   annual: z.boolean().default(true),
-  plan: AllowedValues,
+  plan: z.nativeEnum(PlanOptions).default(PlanOptions.J),
   company: z.string().max(64).min(2),
   createdAt: z.string(),
 });
